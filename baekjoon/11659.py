700ea520684aa10d34c3c7d5f8f7d011c125b1e6
imhynni <omelas329@gmail.com>
2022-08-19 01:11:44 +0900

    백준 11659 <구간 합 구하기 4> - 누적합

---
+++ baekjoon/11659.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/11659
+# 백준 11659 <구간 합 구하기>
+# Silver 3
+
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+num = list(map(int, sys.stdin.readline().split()))
+s_sum = 0
+result = [0]
+
+for i in num:
+    s_sum += i
+    result.append(s_sum)
+for m in range(m):
+    i, j = map(int, sys.stdin.readline().split())
+    print(result[j] - result[i - 1])
