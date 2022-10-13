a3817fb7516b989efcdedb59be279b89db855805
imhynni <omelas329@gmail.com>
2022-10-14 00:00:02 +0900

    백준 2493 <탑> - 스택

---
+++ baekjoon/2493.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/2493
+# 백준 2493 <탑>
+# Gold 5
+import sys
+
+
+def solution():
+    n = int(sys.stdin.readline())
+    heights = list(map(int, sys.stdin.readline().split()))
+    stack = []
+    answer = [0] * n
+    for i in range(n - 1, 0, -1):
+        stack.append((i, heights[i]))
+        while stack and stack[-1][1] <= heights[i - 1]:
+            index, _ = stack.pop()
+            answer[index] = i  # i - 1 + 1
+    print(*answer)
+
+
+solution()
