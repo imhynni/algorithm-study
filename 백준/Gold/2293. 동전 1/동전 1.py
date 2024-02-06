63b140e1e0b3d794b2f7756b0844293fb79efd69
Jeonghyeon <omelas329@gmail.com>
2024-02-06 15:49:02 +0900

    [Gold V] Title: 동전 1, Time: 108 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def solution():
+    n, k = map(int, sys.stdin.readline().split())
+    coins = []
+    for _ in range(n):
+        coins.append(int(sys.stdin.readline()))
+    dp = [0] * (k + 1)
+    dp[0] = 1
+    for coin in coins:
+        for i in range(coin, k + 1):
+            dp[i] += dp[i - coin]
+
+    print(dp[k])
+
+
+solution()
