541e34029761db5fae06753e5fab3c1ff1af5133
Jeonghyeon <omelas329@gmail.com>
2024-10-14 15:19:42 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -5,32 +5,29 @@ min_answer = float('inf')
 max_answer = float('-inf')
 
 
-def backtracking(n, nums, operators, result, total):
+def backtracking(n, nums, operators, count, total):
     global min_answer, max_answer
-    if len(result) == n - 1:
+    if count == n - 1:
         min_answer = min(min_answer, total)
         max_answer = max(max_answer, total)
         return
 
     for i in range(len(operators)):
         if operators[i] > 0:
-            result.append(i)
             operators[i] -= 1
-            curr = len(result)
             temp_total = total
             if i == 0:
-                temp_total += nums[curr]
+                temp_total += nums[count + 1]
             elif i == 1:
-                temp_total -= nums[curr]
+                temp_total -= nums[count + 1]
             elif i == 2:
-                temp_total *= nums[curr]
+                temp_total *= nums[count + 1]
             elif i == 3:
                 if temp_total < 0:
-                    temp_total = -(-temp_total // nums[curr])
+                    temp_total = -(-temp_total // nums[count + 1])
                 else:
-                    temp_total //= nums[curr]
-            backtracking(n, nums, operators, result, temp_total)
-            result.pop()
+                    temp_total //= nums[count + 1]
+            backtracking(n, nums, operators, count + 1, temp_total)
             operators[i] += 1
 
 
@@ -39,7 +36,7 @@ def solution():
     n = int(input())
     nums = list(map(int, input().split()))
     operators = list(map(int, input().split()))
-    backtracking(n, nums, operators, [], nums[0])
+    backtracking(n, nums, operators, 0, nums[0])
     print(max_answer)
     print(min_answer)
 
