849e77400b3820b3ceeb8c223b6b6374ca9add6b
Jeonghyeon <omelas329@gmail.com>
2024-10-16 10:17:36 +0900

    [Gold V] Title: 퇴사 2, Time: 1640 ms, Memory: 252216 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def solution():
+    input = sys.stdin.readline
+    n = int(input())
+    counsels = []
+    for _ in range(n):
+        t, p = map(int, input().split())
+        counsels.append((t, p))
+    dp = [0] * 1_500_001
+    answer = 0
+    for i in range(n - 1, -1, -1):
+        time, price = counsels[i]
+        if i + time > n:
+            dp[i] = dp[i + 1]
+            continue
+        dp[i] = max(dp[i + time] + price, dp[i + 1])
+        answer = max(answer, dp[i])
+
+    print(answer)
+
+
+solution()
