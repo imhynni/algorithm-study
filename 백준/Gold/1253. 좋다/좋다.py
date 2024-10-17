7d31c36763451a7437ae5ba8ac65cfd14376b4f8
Jeonghyeon <omelas329@gmail.com>
2024-10-17 17:55:17 +0900

    [Gold IV] Title: 좋다, Time: 440 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def solution():
+    N = int(sys.stdin.readline())
+    A = list(map(int, sys.stdin.readline().split()))
+    A.sort()
+
+    count = 0
+    for i in range(N):
+        temp_A = A[:i] + A[i+1:]  # A[i] 제외한 리스트
+        start, end = 0, N - 2
+        while start < end:
+            sum_A = temp_A[start] + temp_A[end]
+            if A[i] == sum_A:
+                count += 1
+                break
+            if A[i] > sum_A:
+                start += 1
+            else:
+                end -= 1
+    print(count)
+
+
+solution()
