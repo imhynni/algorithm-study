7e7defb06f6aaa057d4ca6f0acd4ec5334c3ad1c
Jeonghyeon <omelas329@gmail.com>
2024-12-13 01:14:46 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 73.63 ms, Memory: 69.1 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,31 +1,31 @@
 import java.util.*;
 
 class Solution {
-    static Set<Integer>[] columnOil;
+    static List<Set<Integer>> columnOil;
     static List<Integer> oilSize;
     static int n, m;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
-    
+
     static class Node {
         int x, y;
-        
+
         Node(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
-    
+
     public int solution(int[][] land) {
         n = land.length;
         m = land[0].length;
-        columnOil = new Set[m];
+        columnOil = new ArrayList<>();
         oilSize = new ArrayList<>();
-        
+
         for (int j = 0; j < m; j++) {
-            columnOil[j] = new HashSet<>();
+            columnOil.add(new HashSet<>());
         }
-        
+
         int num = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
@@ -36,38 +36,38 @@ class Solution {
                 }
             }
         }
-        
+
         int answer = 0;
-        
+
         for (int j = 0; j < m; j++) {
-            if (columnOil[j].isEmpty()) {
+            if (columnOil.get(j).isEmpty()) {
                 continue;
             }
             int sum = 0;
-            for (int n : columnOil[j]) {
+            for (int n : columnOil.get(j)) {
                 sum += oilSize.get(n);
             }
             answer = Math.max(answer, sum);
         }
-        
+
         return answer;
     }
-    
+
     static int bfs(int[][] land, Node start, int num) {
         int x = start.x;
         int y = start.y;
-        
+
         Queue<Node> q = new ArrayDeque<>();
         q.offer(start);
         land[x][y] = -1;
-        columnOil[y].add(num);
-        
+        columnOil.get(y).add(num);
+
         int count = 1;
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             Node curr = q.poll();
             x = curr.x;
             y = curr.y;
-            
+
             for (int d = 0; d < 4; d++) {
                 int nx = x + dx[d];
                 int ny = y + dy[d];
@@ -77,10 +77,10 @@ class Solution {
                 q.offer(new Node(nx, ny));
                 land[nx][ny] = -1;
                 count++;
-                columnOil[ny].add(num);
+                columnOil.get(ny).add(num);
             }
         }
-        
+
         return count;
     }
 }
