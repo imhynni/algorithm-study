231e9bb8aa431a3f6fe374f9e5d582a5053411c1
Jeonghyeon <omelas329@gmail.com>
2025-02-05 22:55:05 +0900

    [Gold IV] Title: 직사각형 탈출, Time: 832 ms, Memory: 96196 KB -BaekjoonHub

--- 백준/Gold/16973. 직사각형 탈출/직사각형 탈출.java
+++ 백준/Gold/16973. 직사각형 탈출/직사각형 탈출.java
@@ -19,13 +19,19 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[][] area = new int[N][M];
-        boolean[][] visited = new boolean[N][M];
+        int[][] area = new int[N + 1][M + 1];
+        boolean[][] visited = new boolean[N + 1][M + 1];
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
+            for (int j = 1; j <= M; j++) {
                 area[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                area[i][j] += area[i - 1][j] + area[i][j - 1] - area[i - 1][j - 1];
             }
         }
 
@@ -34,10 +40,10 @@ public class Main {
         int W = Integer.parseInt(st.nextToken());
         int[] start = new int[2];
         int[] finish = new int[2];
-        start[0] = Integer.parseInt(st.nextToken()) - 1;
-        start[1] = Integer.parseInt(st.nextToken()) - 1;
-        finish[0] = Integer.parseInt(st.nextToken()) - 1;
-        finish[1] = Integer.parseInt(st.nextToken()) - 1;
+        start[0] = Integer.parseInt(st.nextToken());
+        start[1] = Integer.parseInt(st.nextToken());
+        finish[0] = Integer.parseInt(st.nextToken());
+        finish[1] = Integer.parseInt(st.nextToken());
 
         ArrayDeque<Point> q = new ArrayDeque<>();
         q.add(new Point(start[0], start[1]));
@@ -56,38 +62,22 @@ public class Main {
                     return;
                 }
 
-                Outer: for (int d = 0; d < 4; d++) {
+                for (int d = 0; d < 4; d++) {
                     int nx = curr.x + dx[d];
                     int ny = curr.y + dy[d];
+                    int nh = nx + H - 1;
+                    int nw = ny + W - 1;
 
-                    if (nx < 0 || nx >= N || ny < 0 || ny >= M || visited[nx][ny]) {
+                    if (nx <= 0 || nx > N || ny <= 0 || ny > M || nh <= 0 || nh > N || nw <= 0
+                            || nw > M || visited[nx][ny]) {
                         continue;
                     }
 
-                    if (dx[d] == 0) {
-                        int nc = dy[d] == -1 ? ny : curr.y + W;
+                    int count = area[nh][nw] - area[nh][ny - 1] - area[nx - 1][nw]
+                            + area[nx - 1][ny - 1];
 
-                        if (nc < 0 || nc >= M) {
-                            continue Outer;
-                        }
-
-                        for (int r = 0; r < H; r++) {
-                            if (area[nx + r][nc] == 1) {
-                                continue Outer;
-                            }
-                        }
-                    } else {
-                        int nr = dx[d] == -1 ? nx : curr.x + H;
-
-                        if (nr < 0 || nr >= N) {
-                            continue Outer;
-                        }
-
-                        for (int c = 0; c < W; c++) {
-                            if (area[nr][ny + c] == 1) {
-                                continue Outer;
-                            }
-                        }
+                    if (count != 0) {
+                        continue;
                     }
 
                     q.add(new Point(nx, ny));
