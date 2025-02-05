e8fb95d8790ad64b954e03470b9229e3af245299
Jeonghyeon <omelas329@gmail.com>
2025-02-05 15:57:30 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 77.04 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/README.md
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/README.md
@@ -1,10 +1,10 @@
 # [level 3] [카카오 인턴] 보석 쇼핑 - 67258 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67258?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67258?language=java#) 
 
 ### 성능 요약
 
-메모리: 82.4 MB, 시간: 49.73 ms
+메모리: 83 MB, 시간: 77.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 05일 15:39:47
+2025년 02월 05일 15:57:28
 
 ### 문제 설명
 
--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -2,19 +2,17 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] gems) {
-        HashSet<String> gemType = new HashSet<>();
         HashMap<String, Integer> counter = new HashMap<>();
-        
-        for (String gem : gems) {
-            gemType.add(gem);
-        }
-        
-        int types = gemType.size();
+        int types = new HashSet<>(Arrays.asList(gems)).size();
         int left = 0, right = 0;
         counter.put(gems[0], 1);
-        int[] answer = {0, 100_000};
+        int[] answer = {1, gems.length};
         
-        while (true) {
+        while (right < gems.length - 1) {
+            while (counter.size() < types && right < gems.length - 1) {
+                counter.put(gems[++right], counter.getOrDefault(gems[right], 0) + 1);
+            }
+            
             while (counter.size() == types) {
                 if (right - left < answer[1] - answer[0]) {
                     answer[0] = left + 1;
@@ -28,12 +26,6 @@ class Solution {
                 }
                 
                 left++;
-            }
-            
-            if (counter.size() < types) {
-                if (right + 1 == gems.length)
-                    break;
-                counter.put(gems[++right], counter.getOrDefault(gems[right], 0) + 1);
             }
         }
         
