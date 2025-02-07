be9f1f9cf2d35e7d2fcd37714666597780b53a30
Jeonghyeon <omelas329@gmail.com>
2025-02-08 02:30:03 +0900

    [Gold III] Title: 내리막 길, Time: 344 ms, Memory: 36436 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -2,48 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static final int[] dx = {-1, 1, 0, 0};
+    static final int[] dy = {0, 0, -1, 1};
     static int N, M;
-    static int[][] area;
-    static int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-    static int[][] dp;
+    static int[][] area, dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        area = new int[M][N];
+        dp = new int[M][N];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                area[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = -1;
+            }
+        }
+
+        System.out.println(dfs(0, 0));
+    }
 
     static int dfs(int x, int y) {
-        if (x == N - 1 && y == M - 1) {
+        if (x == M - 1 && y == N - 1) {
             return 1;
         }
+
         if (dp[x][y] > -1) {
             return dp[x][y];
         }
 
         dp[x][y] = 0;
-        for (int[] d : directions) {
-            int nx = x + d[0];
-            int ny = y + d[1];
-            if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= M || ny < 0 || ny >= N || area[x][y] <= area[nx][ny]) {
                 continue;
-            if (area[x][y] <= area[nx][ny])
-                continue;
+            }
+
             dp[x][y] += dfs(nx, ny);
         }
 
         return dp[x][y];
-    }
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        area = new int[N][M];
-        dp = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                area[i][j] = Integer.parseInt(st.nextToken());
-                dp[i][j] = -1;
-            }
-        }
-        
-        System.out.println(dfs(0, 0));
     }
 }
